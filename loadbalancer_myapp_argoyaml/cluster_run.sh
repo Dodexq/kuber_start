fd390e7c63a057789b9f8bcdb5e8c57c5b2e35c7
Dodex <dodexpwnz@gmail.com>
2023-01-22 18:03:08 +0300

    upd 22.01

--- loadbalancer_myapp_argoyaml/cluster_run.sh
+++ loadbalancer_myapp_argoyaml/cluster_run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo kind create cluster --name cluster01 --config cluster01-kind.yml --image=kindest/node:v1.21.2
+sudo kind create cluster --name cluster01 --config cluster01-kind.yaml --image=kindest/node:v1.21.2
