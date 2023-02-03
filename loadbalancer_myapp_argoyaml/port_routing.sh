f17b037f86462a7705b4fd9a96ee7c83c9419054
Dodex <dodexpwnz@gmail.com>
2023-02-03 20:59:35 +0300

    ingress

--- loadbalancer_myapp_argoyaml/port_routing.sh
+++ loadbalancer_myapp_argoyaml/port_routing.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-sudo iptables -t nat -A  DOCKER -p tcp --dport 4545 -j DNAT --to-destination 172.18.0.150:4545
+sudo iptables -t nat -A PREROUTING -p tcp --dport 4545 -j DNAT --to-destination 172.18.0.150:4545
+sudo iptables -t nat -A POSTROUTING -p tcp -d 172.18.0.150 --dport 4545 -j SNAT --to-source 192.168.56.170:4545
