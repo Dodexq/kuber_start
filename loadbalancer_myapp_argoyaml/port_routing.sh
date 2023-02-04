1b8d703cb40bb5725fe6b02afce2750564f5d9a3
Dodex <dodexpwnz@gmail.com>
2023-02-04 12:55:50 +0300

    docker image:v2

--- loadbalancer_myapp_argoyaml/port_routing.sh
+++ loadbalancer_myapp_argoyaml/port_routing.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 sudo iptables -t nat -A PREROUTING -p tcp --dport 4545 -j DNAT --to-destination 172.18.0.150:4545
-sudo iptables -t nat -A POSTROUTING -p tcp -d 172.18.0.150 --dport 4545 -j SNAT --to-source 192.168.56.170:4545
+sudo iptables -t nat -A POSTROUTING -j MASQUERADE
+
+# or
+#sudo iptables -t nat -A POSTROUTING -p tcp -d 172.18.0.150 --dport 4545 -j SNAT --to-source 192.168.56.170:4545
