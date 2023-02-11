036cc079d1104a67df99c6859af1fc38a762ce03
Dodex <dodexpwnz@gmail.com>
2023-02-11 22:35:27 +0300

    update calico&metallb latest version

--- my_cluster/cluster_run.sh
+++ my_cluster/cluster_run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-sudo kind create cluster --name cluster01 --config cluster01-kind.yaml --image=kindest/node:v1.21.2
+sudo kind create cluster --name cluster01 --config cluster01-kind.yaml 
+#--image=kindest/node:v1.21.2
